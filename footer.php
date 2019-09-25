042e99e8cfd324eb2dfca962af06ca6a92dde139
shuvo <shuvoislamcmt3@gmail.com>
2019-09-25 18:08:26 +0600

    full page markup complete

--- footer.php
+++ footer.php
@@ -1,96 +1,95 @@
 <?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package understrap
- */
-
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-
+    /**
+     * The template for displaying the footer.
+     *
+     * Contains the closing of the #content div and all content after
+     *
+     * @package understrap
+     */
+    
+    // Exit if accessed directly.
+    defined( 'ABSPATH' ) || exit;
+    
 $container = get_theme_mod( 'understrap_container_type' );
-?>
-
+    ?>
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
-
 <div class="wrapper" id="wrapper-footer">
-
-	<div class="<?php echo esc_attr( $container ); ?>">
-		
-		<footer class="site-footer" id="colophon">
-		
-		<div class="footer-top">
-			<div class="row">
-				<div class="col-md-12">
-					<?php echo do_shortcode('[email-subscribers-form id="1"]')?>
-				</div>
-			</div>
-
-			<div class="row account-create-area">
-				<div class="col-md-6">
-					<h3>Ready to get started?</h3>
-					<p>Get in touch or create an account.</p>
-				</div>
-				<div class="col-md-6">
-					<a href="" class="green-btn"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-greem.png" alt=""></a>
-					<a href="" class="orange-btn"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-orange.png" alt=""></a>
-				</div>
-			</div>
-
-			<div class="row contact-information text-center">
-				<div class="col-md-3">
-					<i class="fa fa-phone"></i>
-					<h6>Phone</h6>
-					<p>(+428) 888 888 8888</p>
-				</div>
-				<div class="col-md-3">
-					<i class="fa fa-map-marker"></i>
-					<h6>Head Office</h6>
-					<p>123 west Dummy Car Street, Old City,New State, USA. PO 4288</p>
-				</div>
-				<div class="col-md-3">
-					<i class="fa fa-map-marker"></i>
-					<h6>Main Office</h6>
-					<p>123 west Dummy Car Street, Old City,New State, USA. PO 4288</p>
-				</div>
-				<div class="col-md-3">
-					<i class="fa fa-envelope"></i>
-					<h6>Email</h6>
-					<p>info@paygate.com</p>
-				</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="col-md-6">
-				<div class="site-info">
-					<p>Copyrights © 2019, Paygate.All rights reserved  |  Privacy policy  |  Terms of use</p>
-				</div><!-- .site-info -->
-			</div><!--col end -->
-			<div class="col-md-6">
-				<div class="site-footer-social">
-					<ul>
-						<li><a href=""><i class="fa fa-facebook"></i></a></li>
-						<li><a href=""><i class="fa fa-twitter"></i></a></li>
-						<li><a href=""><i class="fa fa-pinterest"></i></a></li>
-						<li><a href=""><i class="fa fa-youtube-play"></i></a></li>
-					</ul>
-				</div><!-- .site-info -->
-			</div><!--col end -->
-		</div><!-- row end -->
-		</footer><!-- #colophon -->
-
-	</div><!-- container end -->
-
-</div><!-- wrapper end -->
-
+    <div class="footer-top" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-copy-2.png');">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 newslatter-area">
+                    <div class="section-title">
+                        <h2><strong>Sign Up</strong> Newsletter</h2>
+                        <p>In ullamcorper est eget tempor cursus neque mi consectetuer</p>
+                    </div>
+                    <?php echo do_shortcode('[email-subscribers-form id="1"]')?>
+                </div>
+            </div>
+            <div class="row account-create-area">
+                <div class="col-md-6">
+                    <h3>Ready to get started?</h3>
+                    <p>Get in touch or create an account.</p>
+                </div>
+                <div class="col-md-6 text-right footer-btn">
+                    <a href="" class="green-btn-footer"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-greem.png" alt=""></a>
+                    <a href="" class="orange-btn-footer"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-orange.png" alt=""></a>
+                </div>
+            </div>
+            <div class="row contact-information text-center">
+                <div class="col-md-3">
+                    <i class="fa fa-phone"></i>
+                    <h6>Phone</h6>
+                    <p>(+428) 888 888 8888</p>
+                </div>
+                <div class="col-md-3">
+                    <i class="fa fa-map-marker"></i>
+                    <h6>Head Office</h6>
+                    <p>123 west Dummy Car Street, Old City,New State, USA. PO 4288</p>
+                </div>
+                <div class="col-md-3">
+                    <i class="fa fa-map-marker"></i>
+                    <h6>Main Office</h6>
+                    <p>123 west Dummy Car Street, Old City,New State, USA. PO 4288</p>
+                </div>
+                <div class="col-md-3">
+                    <i class="fa fa-envelope"></i>
+                    <h6>Email</h6>
+                    <p>info@paygate.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="<?php echo esc_attr( $container ); ?>">
+        <footer class="site-footer" id="colophon">
+            <div class="row footer-copyright">
+                <div class="col-md-8">
+                    <div class="site-info">
+                        <p>Copyrights © 2019, Paygate.All rights reserved  |  Privacy policy  |  Terms of use</p>
+                    </div>
+                    <!-- .site-info -->
+                </div>
+                <!--col end -->
+                <div class="col-md-4">
+                    <div class="site-footer-social">
+                        <ul>
+                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href=""><i class="fa fa-youtube-play"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- .site-info -->
+                </div>
+                <!--col end -->
+            </div>
+            <!-- row end -->
+        </footer>
+        <!-- #colophon -->
+    </div>
+    <!-- container end -->
+</div>
+<!-- wrapper end -->
 </div><!-- #page we need this extra closing tag here -->
-
 <?php wp_footer(); ?>
-
 </body>
-
 </html>
-
--- page-templates/home.php
+++ page-templates/home.php
@@ -12,11 +12,15 @@ defined( 'ABSPATH' ) || exit;
 
 get_header(); ?>
 
+<div class="slider-area">
+	<?php echo do_shortcode('[rev_slider alias="slider-1"][/rev_slider]')?>
+</div>
+
 <div class="focus-business-area">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 text-right">
-				<h2>FOCUS ON YOUR BUSINESS</h2>
+				<h2>FOCUS ON YOUR <strong>BUSINESS</strong></h2>
 			</div>
 			<div class="col-md-2">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Group-55.png" alt="">
@@ -28,13 +32,13 @@ get_header(); ?>
 	</div>
 </div>
 
-<div class="pci-area">
-	<div class="container">
+<div class="pci-area" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg1.png');">
+	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-6 pci-area-left">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg-copy.png" alt="">
 			</div>
-			<div class="col-md-6 text-right">
+			<div class="col-md-6 text-right pci-area-right">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Group-2.png" alt="">
 				<p>We are PCI certified by the standard known as PCI DSS (Payment Card Industry Data Security Standard) founded by various institutions including Visa and Mastercard. We have been certified in level 3 as a merchant and we are finishing the process for level 2 as a Service Provider coming in Agoust 2019.</p>
 				<a href="" class="orange-btn"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-orange.png" alt=""></a>
@@ -43,30 +47,34 @@ get_header(); ?>
 	</div>
 </div>
 
-<div class="what-we-do">
+
+
+<div class="what-we-do section-padding" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Layer-36.png');">
 	<div class="container">
 		<div class="row text-center">
-			<div class="section-title">
-				<h2>What <strong>we Do</strong></h2>
+			<div class="col-md-12">
+				<div class="section-title">
+					<h2>What <strong>we Do</strong></h2>
+				</div>
 			</div>
 		</div>
-		<div class="row what-we-do-single">
+		<div class="row what-we-do-single what-we-do-single-one">
 			<div class="col-md-6 text-right">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Group-54.png" alt="">
 				<h3>Sed ut perspiciati</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqt enim ut aliquip ex ea commodo consequat</p>
 				<a href="" class="orange-btn"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-orange.png" alt=""></a>
 			</div>
-			<div class="col-md-6">
+			<div class="col-md-6 ww-img">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg2.png" alt="">
 			</div>
 		</div>
 		<div class="row what-we-do-single">
-			<div class="col-md-6 text-right">
+			<div class="col-md-6 text-right ww-img">
 				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/bg3.png" alt="">
 			</div>
 			<div class="col-md-6 text-left">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Group-54.png" alt="">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Group-541.png" alt="">
 				<h3>Sed ut perspiciati</h3>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqt enim ut aliquip ex ea commodo consequat</p>
 				<a href="" class="orange-btn"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/button-orange.png" alt=""></a>
@@ -74,6 +82,60 @@ get_header(); ?>
 		</div>
 	</div>
 </div>
+
+
+   <!-- testimonial area start-->
+   
+   <div class="testimonial-area section-padding">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="section-title">
+						<h2>What <strong>Clients Say</strong></h2>
+					</div>
+               </div>
+           </div>
+           <div class="row">
+               <div class="col-md-4">
+                   <div class="single-testimonial-box">
+                       <div class="testimonial-text">
+                           <i class="fa fa-quote-left quote-icon"></i>
+                           <p>His wife found him unconscious on the ground, with blood everywhere must have been out for only a fewseconds because, when I came to again, I was still laughing at Jonah.</p>
+                       </div>
+                       <div class="testimonial-bottom">
+                           <img src="images/testimonial-img.png" alt="">
+                            <p class="author-details"><strong>Alva Dickinson</strong><br />CEO, Craftytheem</p>
+                        </div>
+                   </div>
+               </div>
+               <div class="col-md-4">
+                   <div class="single-testimonial-box active">
+                       <div class="testimonial-text">
+                           <i class="fa fa-quote-left quote-icon"></i>
+                           <p>His wife found him unconscious on the ground, with blood everywhere must have been out for only a fewseconds because, when I came to again, I was still laughing at Jonah.</p>
+                       </div>
+                       <div class="testimonial-bottom">
+                            <img src="images/testimonial-img.png" alt="">
+                            <p class="author-details"><strong>Alva Dickinson</strong><br />CEO, Craftytheem</p>
+                        </div>
+                    </div>
+               </div>
+               <div class="col-md-4">
+                   <div class="single-testimonial-box">
+                       <div class="testimonial-text">
+                           <i class="fa fa-quote-left quote-icon"></i>
+                           <p>His wife found him unconscious on the ground, with blood everywhere must have been out for only a fewseconds because, when I came to again, I was still laughing at Jonah.</p>
+                       </div>
+                       <div class="testimonial-bottom">
+                           <img src="images/testimonial-img.png" alt="">
+                            <p class="author-details"><strong>Alva Dickinson</strong><br />CEO, Craftytheem</p>
+                        </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   <!-- testimonial area ends-->
 
 <?php get_footer();
 
